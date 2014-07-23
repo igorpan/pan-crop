e24a02b0959d78e3341f6d2c525cfc4dd31aec46
Igor Pantović <igor.pantovic@boutsourcing.com>
2014-07-23 13:52:55 +0200

    Added scaling function

--- jquery.pan-crop.js
+++ jquery.pan-crop.js
@@ -1,5 +1,12 @@
 (function($) {
 
+    var limitValue = function (value, min, max) {
+        value = Math.min(max, value);
+        value = Math.max(min, value);
+
+        return value;
+    };
+
     $.fn.panCrop = function(method) {
 
         var methods = {
@@ -8,85 +15,78 @@
                 return this.each(function() {
                     var $element = $(this),
                         settings = $element.panCrop.settings;
-                    
-                    var $wrapper = $('<div/>')
-                        .css({
-                            width: settings.width,
-                            height: settings.height,
-                            overflow: 'hidden'
+
+                    loadOriginalImageSize($element[0].src, function (originalImageSize) {
+                        var $wrapper = $('<div/>')
+                            .css({
+                                width: settings.width,
+                                height: settings.height,
+                                overflow: 'hidden'
+                            });
+                        $element.panCrop.$wrapper = $wrapper;
+
+                        $element.panCrop.cssBackup = {};
+                        $element.panCrop.cssBackup.position = $element[0].style.position;
+                        $element.panCrop.cssBackup.top      = $element[0].style.top;
+                        $element.panCrop.cssBackup.left     = $element[0].style.left;
+                        $element.css({
+                            position: 'relative',
+                            top: 0,
+                            left: 0
                         });
-                    $element.panCrop.$wrapper = $wrapper;
 
-                    $element.panCrop.cssBackup = {};
-                    $element.panCrop.cssBackup.position = $element[0].style.position;
-                    $element.panCrop.cssBackup.top      = $element[0].style.top;
-                    $element.panCrop.cssBackup.left     = $element[0].style.left;
-                    $element.css({
-                        position: 'relative',
-                        top: 0,
-                        left: 0
-                    });
+                        $element.panCrop.crop = {
+                            x1: 0,
+                            y1: 0,
+                            w: settings.width,
+                            h: settings.height,
+                            s: Math.max(Math.max(1, settings.width / originalImageSize.w), settings.height / originalImageSize.h),
+                            originalSize: originalImageSize
+                        };
+                        syncViewToState($element, $element.panCrop.crop);
 
-                    var dragging = false;
-                    var startX   = 0;
-                    var startY   = 0;
+                        var dragging = false;
+                        var startX   = 0;
+                        var startY   = 0;
 
-                    $element.off('mousedown.pancrop');
-                    $element.on('mousedown.pancrop', function (e) {
-                        e.preventDefault();
+                        $element.off('mousedown.pancrop');
+                        $element.on('mousedown.pancrop', function (e) {
+                            e.preventDefault();
 
-                        dragging = true;
-                        startX = e.pageX;
-                        startY = e.pageY;
-                    });
-
-                    $(document).off('mouseup.pancrop');
-                    $(document).on('mouseup.pancrop', function () {
-                        dragging = false;
-                    });
-
-                    $element.on('mousemove.pancrop', function (e) {
-                        if (dragging) {
-                            var deltaX = e.pageX - startX;
-                            var deltaY = e.pageY - startY;
+                            dragging = true;
                             startX = e.pageX;
                             startY = e.pageY;
-                            
-                            var currentLeft = parseInt($element.css('left'));
-                            var currentTop = parseInt($element.css('top'));
+                        });
 
-                            var left = limitValue(currentLeft + deltaX, settings.width - $element.width(), 0);
-                            var top = limitValue(currentTop + deltaY, settings.height - $element.height(), 0);
+                        $(document).off('mouseup.pancrop');
+                        $(document).on('mouseup.pancrop', function () {
+                            dragging = false;
+                        });
 
-                            $element.css({
-                                left: left,
-                                top: top
-                            });
+                        $element.on('mousemove.pancrop', function (e) {
+                            if (dragging) {
+                                var deltaX = e.pageX - startX;
+                                var deltaY = e.pageY - startY;
+                                startX = e.pageX;
+                                startY = e.pageY;
+                                var cropValues = $element.panCrop.crop;
 
-                            var cropValues = {
-                                x1: - left,
-                                y1: - top,
-                                x2: - left + settings.width,
-                                y2: - top + settings.height,
-                                w: settings.width,
-                                h: settings.height
-                            };
-                            var originalImageSize = getOriginalImageSize($element[0].src);
-                            var xMod = originalImageSize.w / $element.width();
-                            var yMod = originalImageSize.h / $element.height();
+                                var left = limitValue(- cropValues.x1 + deltaX, settings.width - $element.width(), 0);
+                                var top = limitValue(- cropValues.y1 + deltaY, settings.height - $element.height(), 0);
+                                
+                                cropValues.x1 = - left;
+                                cropValues.y1 = - top;
 
-                            cropValues.x1 = cropValues.x1 * xMod;
-                            cropValues.x2 = cropValues.x2 * xMod;
-                            cropValues.w  = cropValues.w  * xMod;
-                            cropValues.y1 = cropValues.y1 * yMod;
-                            cropValues.y2 = cropValues.y2 * yMod;
-                            cropValues.h  = cropValues.h  * yMod;
+                                syncViewToState($element, cropValues);
+                            }
+                        });
 
-                            $element.panCrop.crop = cropValues;
+                        $element.wrap($wrapper);
+
+                        if (settings.onLoad) {
+                            settings.onLoad();
                         }
                     });
-
-                    $element.wrap($wrapper);
                 });
             },
 
@@ -101,24 +101,53 @@
                     $element.unwrap();
                     $element.unbind('mousemove.pancrop mouseup.pancrop mousedown.pancrop');
                 }
+            },
+
+            scale : function (scale) {
+                var $element = $(this);
+                var settings = $element.panCrop.settings;
+                var cropValues = $element.panCrop.crop;
+                var originalSize = $element.panCrop.crop.originalSize;
+
+                var oldWidth  = $element.width();
+                var oldHeight = $element.height();
+
+                cropValues.s = limitValue(scale, settings.width / originalSize.w, Math.max(1, settings.width / originalSize.w));
+                syncViewToState($element, cropValues);
+
+                var widthRatio  = $element.width() / oldWidth;
+                var heightRatio = $element.height() / oldHeight;
+
+                var newX1 = (cropValues.x1 + settings.width / 2) * widthRatio - settings.width / 2;
+                var newY1 = (cropValues.y1 + settings.height / 2) * heightRatio - settings.height / 2;
+
+                cropValues.x1 = limitValue(newX1, 0, $element.width() - settings.width);
+                cropValues.y1 = limitValue(newY1, 0, $element.height() - settings.height);
+                syncViewToState($element, cropValues);
+
+                return cropValues.s;
             }
         };
 
-        var limitValue = function (value, min, max) {
-            value = Math.min(max, value);
-            value = Math.max(min, value);
-
-            return value;
+        var syncViewToState = function ($element, stateObj) {
+            var originalSize = $element.panCrop.crop.originalSize;
+            $element.width(originalSize.w * stateObj.s);
+            $element.height(originalSize.h * stateObj.s);
+            $element.css({
+                left: - stateObj.x1,
+                top: - stateObj.y1
+            });
         };
 
-        var getOriginalImageSize = function (src) {
+        var loadOriginalImageSize = function (src, callback) {
             var img = new Image();
-            img.src = src;
-
-            return {
-                w: img.width,
-                h: img.height
+            img.onload = function () {
+                callback({
+                    w: img.width,
+                    h: img.height
+                });
             };
+            img.src = src;
         };
 
         if (methods[method]) {
